3750d4bbda75b7144efe17dcdf44fb3fdbfcb2e6
Kay Zeng <kayzeng777@gmail.com>
2025-09-29 00:57:38 -0700

    use correct token

--- src/lib/mixpanel.ts
+++ src/lib/mixpanel.ts
@@ -1,6 +1,6 @@
 import mixpanel from "mixpanel-browser";
 
-mixpanel.init("372c1b5d9565897bd8c52f8dbee60ce7", {
+mixpanel.init("2bb8d08521ad815b6a3d0f015a12bfa2", {
     record_sessions_percent: 100,
     track_pageview: true,
     ignore_dnt: true,
